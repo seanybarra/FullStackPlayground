3b6948714576296c3b60586879ee1ebcf932b923
seanybarra <sean_ybarra@yahoo.com>
2023-06-03 13:38:54 -0700

    Added server listen statement

--- chap02/00-helloworld.js
+++ chap02/00-helloworld.js
@@ -5,3 +5,6 @@ const server = http.createServer((req,res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.end('Hello World!')
 })
+
+server.listen(port, () => console.log(`Server started on port ${port}; ` + 
+'press Ctrl - C to terminate...'))
