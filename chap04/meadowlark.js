d73ac3dcfce5a368d510e77e0df4cc172c1146c8
seanybarra <sean_ybarra@yahoo.com>
2023-06-10 13:22:00 -0700

    Add project from chapter 4

---
+++ chap04/meadowlark.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const fortune = require('./lib/fortune.js')
+
+const app = express()
+
+// configure Handlebars view engine
+app.engine('handlebars',expressHandlebars.engine({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname+'/public'))
+
+app.get('/', (req,res) =>{
+    res.render('home')
+})
+
+app.get('/about', (req,res) => {
+    res.render('about', {fortune: fortune.getFortune() })
+})
+
+// Custom 404 page
+app.use((req, res) => {
+    res.status(404)
+    res.render('404')
+})
+
+// Custom 500 page
+app.use((err, req, res, next) => {
+    console.log(err.message)
+    res.status(500)
+    res.render('500')
+})
+
+app.listen(port, () => {
+    console.log(`Express started on http://localhost:${port}` +
+    '; press Ctrl-C to termincate..')
+})
