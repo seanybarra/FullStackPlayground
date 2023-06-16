1fdf620224f1f60f54bfc84815715b9ae140997e
seanybarra <sean_ybarra@yahoo.com>
2023-06-15 20:14:40 -0700

    Add 05-view-with-content.js

--- chap06/05-view-with-content.js
+++ chap06/05-view-with-content.js
@@ -13,3 +13,32 @@ app.set('view engine', 'handlebars')
 
 // the following is needed for cookie support
 app.use(cookieParser())
+
+// The following is needed for session support
+app.use(session({ resave: false, saveUninitialized: false, secret: 'keyboard cat' }))
+
+
+// See the views/greeting.handlebars file for the contents of this view
+app.get('/greeting', (req,res) => {
+    res.render('/greeting', {
+        message: 'Hello esteemed programmer',
+        style: req.query.style,
+        userid: req.cookies.userid,
+        username: req.session.username
+    })
+})
+
+app.get('/set-random-userid', (req,res) => {
+    res.cookie('userid', (Math.random()*10000).toFixed(0))
+    res.redirect('/greeting')
+})
+
+app.get('/set-random-username', (req,res) => {
+    res.session.username = catNames.random()
+    res.redirect('/greeting')
+})
+
+app.get('*', (req,res) => res.send('Check out our <a href="/greeting">greeting</a> page!'))
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`\nNavigate to  http://localhost:${port}/greeting\n`))
