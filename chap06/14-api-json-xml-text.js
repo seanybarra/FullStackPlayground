0a662f062feecb35b88f1909f7ac67da7df55e46
seanybarra <sean_ybarra@yahoo.com>
2023-06-18 12:27:35 -0700

    Add 14-api-json-xml-text

---
+++ chap06/14-api-json-xml-text.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+const tours = [
+    {id: 0, name: 'Hood River', price: 99.99},
+    {id: 1, name: 'Oergon Coast', price: 149.95},
+]
+
+app.get('/api/tours', (req,res) => {
+    const toursXml = '<?xml version="1.0"?><tours>' +
+    tours.map(p => `<tour price="${p.price}" id="${p.id}">${p.name}</tour>`).join('') + '</tours>'
+    const toursText = tours.map(p => `${p.id}: ${p.name} (${p.price})`).join('\n')
+    res.format({
+        'application/json': () => res.json(tours),
+        'application/xml': () => res.type('application/xml').send(toursXml),
+        'text/xml': () => res.type('text/xml').send(toursXml),
+        'text/plain': () => res.type('text/plain').send(toursText),
+    })
+})
+
+app.use('*', (req,res) => res.send('checkout <a href="/api/tours">/api/tours</a>!'))
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`\nNavigate to http://localhost:${port}\n`))
