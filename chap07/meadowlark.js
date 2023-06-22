e0da69f4e343a1f11b1d20cdcd1f6efcfb24f3e5
seanybarra <sean_ybarra@yahoo.com>
2023-06-22 16:33:05 -0700

    Fix sections error

--- chap07/meadowlark.js
+++ chap07/meadowlark.js
@@ -7,7 +7,9 @@ app.engine('handlebars', expressHandlebars.engine({
     defaultLayout: 'main',
     helpers: {
         section: function(name,options){
-            if(!this._sections) this._sections = {}
+            if(!this._sections){
+                this._sections = {}
+            }
             this._sections[name] = options.fn(this)
             return null
         },
