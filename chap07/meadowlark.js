ade2eae44d5c34ee6ee833369037cb31fc1ade09
seanybarra <sean_ybarra@yahoo.com>
2023-06-21 19:08:53 -0700

    add more to layour main, create section-test view, added more to meadowlark.js

--- chap07/meadowlark.js
+++ chap07/meadowlark.js
@@ -4,6 +4,18 @@ const expressHandlebars = require('express-handlebars')
 const app = express()
 
 app.engine('handlebars', expressHandlebars.engine({
-    defaultLayout: 'main'
+    defaultLayout: 'main',
+    helpers: {
+        section: function(name,options){
+            if(!this._sections) this._sections = {}
+            this._sections[name] = options.fn(this)
+            return null
+        },
+    },
 }))
 app.set('view engine', 'handlebars')
+const port = process.env.PORT || 3000
+
+app.get('/section-test', (req,res) => res.render('section-test'))
+
+app.listen(port, () => console.log(`Express started on http://localhost:${port}` + '; press Ctrl-C to terminate'))
